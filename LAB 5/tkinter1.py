28fb2a3024c11866d2f68a2acf720192d6eb1cab
altheantonio <161104981+altheantonio@users.noreply.github.com>
2024-04-28 00:09:22 +0800

    Delete LAB 5 directory

--- LAB 5/tkinter1.py
+++
@@ -1,6 +0,0 @@
-from tkinter import filedialog
-from tkinter import *
-
-root = Tk()
-root.filename =  filedialog.askopenfilename(title = "Select file",filetypes = (("text files","*.txt")))
-print (root.filename)
