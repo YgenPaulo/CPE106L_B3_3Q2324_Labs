1ed9d02bacb4442cc80f75401a76cbc5e2411008
YgenPaulo <160853446+YgenPaulo@users.noreply.github.com>
2024-04-27 21:59:45 +0800

    lab 3

---
+++ lab3/generator_modified.py
@@ -0,0 +1,41 @@
+import random
+
+
+articles = ("A", "THE")
+nouns = ("BOY", "GIRL", "BAT", "BALL")
+verbs = ("HIT", "SAW", "LIKED")
+prepositions = ("WITH", "BY")
+
+def getWords(fileName):
+    with open("articles.txt", "r") as fileName:
+        word = [word.strip() for word in fileName]
+        return tuple(word)
+    
+def sentence():
+    """Builds and returns a sentence."""
+    return nounPhrase() + " " + verbPhrase()
+
+def nounPhrase():
+    """Builds and returns a noun phrase."""
+    return random.choice(articles) + " " + random.choice(nouns)
+
+def verbPhrase():
+    """Builds and returns a verb phrase."""
+    return random.choice(verbs) + " " + nounPhrase() + " " + prepositionalPhrase()
+
+def prepositionalPhrase():
+    """Builds and returns a prepositional phrase."""
+    return random.choice(prepositions) + " " + nounPhrase()
+    
+
+def main():
+    number = int(input("Enter the number of sentences: "))
+    for count in range(number):
+        print(sentence())
+# The entry point for program execution
+if __name__ == "__main__":
+    main()
+    articles = getWords("articles.txt")
+    nouns = getWords("nouns.txt")
+    verbs = getWords("verbs.txt")
+    prepositions = getWords("prepositions.txt")
