f4652d83f9fdd030d2d2bff4be064df204a5c1b8
EthanesWong <161921354+EthanesWong@users.noreply.github.com>
2024-04-28 21:12:33 +0800

    Update students (1).py

--- LAB 3/students (1).py
+++ LAB 3/students (1).py
@@ -4,37 +4,29 @@ Resources to manage a student's name and test scores.
 """
 import random
 class Student(object):
-    """Represents a student."""
 
     def __init__(self, name, number):
-        """All scores are initially 0."""
         self.name = name
         self.scores = []
         for count in range(number):
             self.scores.append(0)
 
     def getName(self):
-        """Returns the student's name."""
         return self.name
   
     def setScore(self, i, score):
-        """Resets the ith score, counting from 1."""
         self.scores[i - 1] = score
 
     def getScore(self, i):
-        """Returns the ith score, counting from 1."""
         return self.scores[i - 1]
    
     def getAverage(self):
-        """Returns the average score."""
         return sum(self.scores) / len(self._scores)
     
     def getHighScore(self):
-        """Returns the highest score."""
         return max(self.scores)
  
     def __str__(self):
-        """Returns the string representation of the student."""
         return "Name: " + self.name  + "\nScores: " + \
                " ".join(map(str, self.scores))
     def __eq__(self, other):
@@ -48,26 +40,25 @@ class Student(object):
 
 def main():
     listofstudents = []
-    """A simple test."""
-    student = Student("Ken", 5)
+    student = Student("Ethanes", 5)
     for i in range(1, 6):
         student.setScore(i, 100)
     listofstudents.append(student)
 
-    Axcel = Student("Axcel", 4)
+    Ygen = Student("Ygen", 4)
     for i in range(1, 5):
-        Axcel.setScore(i,100)
-    listofstudents.append(Axcel)
+        Ygen.setScore(i,100)
+    listofstudents.append(Ygen)
     
-    Axcel2 = Student("Axcel", 4)
+    Althea = Student("Althea", 4)
     for i in range(1, 5):
         Axcel2.setScore(i,100)
-    listofstudents.append(Axcel2)
+    listofstudents.append(Althea)
         
-    Nigel = Student("Nigel", 3)
+    Wong = Student("Wong", 3)
     for i in range(1, 4):
         Nigel.setScore(i,100)
-    listofstudents.append(Nigel)
+    listofstudents.append(Wong)
 
     random.shuffle(listofstudents)
     print("shuffled", end="->")
