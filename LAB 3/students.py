8bf2e9c03c4a16b05a8c7e4636b82f89db790119
EthanesWong <161921354+EthanesWong@users.noreply.github.com>
2024-04-28 21:15:07 +0800

    Update students.py

--- LAB 3/students.py
+++ LAB 3/students.py
@@ -4,37 +4,29 @@ Resources to manage a student's name and test scores.
 """
 
 class Student(object):
-    """Represents a student."""
 
     def __init__(self, name, number):
-        """All scores are initially 0."""
         self.name = name
         self.scores = []
         for count in range(number):
             self.scores.append(0)
 
     def getName(self):
-        """Returns the student's name."""
         return self.name
   
     def setScore(self, i, score):
-        """Resets the ith score, counting from 1."""
         self.scores[i - 1] = score
 
     def getScore(self, i):
-        """Returns the ith score, counting from 1."""
         return self.scores[i - 1]
    
     def getAverage(self):
-        """Returns the average score."""
         return sum(self.scores) / len(self._scores)
     
     def getHighScore(self):
-        """Returns the highest score."""
         return max(self.scores)
  
     def __str__(self):
-        """Returns the string representation of the student."""
         return "Name: " + self.name  + "\nScores: " + \
                " ".join(map(str, self.scores))
     def __eq__(self, other):
@@ -47,31 +39,30 @@ class Student(object):
         return self.name >= other.name
 
 def main():
-    """A simple test."""
-    student = Student("Ken", 5)
+    student = Student("Ethanes", 5)
     for i in range(1, 6):
         student.setScore(i, 100)
     print(student)
 
-    Axcel = Student("Axcel", 4)
+    Ygen = Student("Ygen", 4)
     for i in range(1, 5):
         student.setScore(i,100)
-    print(Axcel)
+    print(Ygen)
     
-    Axcel2 = Student("Axcel", 4)
+    Althea = Student("Althea", 4)
     for i in range(1, 5):
         student.setScore(i,100)
-    print(Axcel2)
+    print(Althea)
     
     print("First COMPARISON")
-    print(f"Equal Comparison: {Axcel == student}")
-    print(f"Less Than Comparison: {Axcel < student}")
-    print(f"Greater Than Equal to Comparison: {Axcel >= student}")
+    print(f"Equal Comparison: {Ygen == student}")
+    print(f"Less Than Comparison: {Ygen < student}")
+    print(f"Greater Than Equal to Comparison: {Ygen >= student}")
     
     print("SECOND COMPARISON")
-    print(f"Equal Comparison: {Axcel == Axcel2}")
-    print(f"Less Than Comparison: {Axcel < Axcel2}")
-    print(f"Greater Than Equal to Comparison: {Axcel >= Axcel2}")
+    print(f"Equal Comparison: {Ygen == Althea}")
+    print(f"Less Than Comparison: {Ygen < Althea}")
+    print(f"Greater Than Equal to Comparison: {Ygen >= Althea}")
 
 if __name__ == "__main__":
     main()
