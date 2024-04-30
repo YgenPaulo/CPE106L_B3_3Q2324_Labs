a5fd9cc9af991ec5aed945ef188ab62fd7b392a8
EthanesWong <161921354+EthanesWong@users.noreply.github.com>
2024-04-30 15:00:05 +0800

    Update generator_modified.py

--- LAB 2/generator_modified.py
+++ LAB 2/generator_modified.py
@@ -11,19 +11,15 @@ def getWords(fileName):
         return tuple(word)
     
 def sentence():
-    """Builds and returns a sentence."""
     return nounPhrase() + " " + verbPhrase()
 
 def nounPhrase():
-    """Builds and returns a noun phrase."""
     return random.choice(articles) + " " + random.choice(nouns)
 
 def verbPhrase():
-    """Builds and returns a verb phrase."""
     return random.choice(verbs) + " " + nounPhrase() + " " + prepositionalPhrase()
 
 def prepositionalPhrase():
-    """Builds and returns a prepositional phrase."""
     return random.choice(prepositions) + " " + nounPhrase()
     
 
@@ -31,7 +27,6 @@ def main():
     number = int(input("Enter the number of sentences: "))
     for count in range(number):
         print(sentence())
-# The entry point for program execution
 if __name__ == "__main__":
     main()
     articles = getWords("articles.txt")
